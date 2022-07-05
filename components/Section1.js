4190760b852997faa9681fb9db3680848ec15bb4
Laylson Sousa <laylsonss3@gmail.com>
2022-07-05 00:24:48 -0300

    'update'

--- components/Section1.js
+++ components/Section1.js
@@ -9,7 +9,7 @@ function Section1() {
         <div className='Component flex flex-col mt-8'>
           <h1 className='text-5xl text-white Poppins font-bold'>HÁ 12 ANOS TRANSFORMANDO VIDAS ATRAVÉS DA ARTE.</h1>
           <div className='flex'>
-            <Link href="/matricula">
+            <Link href="#matricula">
               <a className='Poppins hover:text-zinc-300 font-medium text-2xl mt-4 py-2 px-4 bg-emerald-500 hover:bg-emerald-600 transition-colors rounded'>Quero ser aluno</a></Link>
           </div>
         </div>
